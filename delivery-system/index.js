df6cd9f6bcd19ad72392e5064d245cc431644e89
Felipe Gadelha Lino <felipegadelha2004@gmail.com>
2024-03-24 15:25:07 -0300

    adds

--- delivery-system/index.js
+++ delivery-system/index.js
@@ -2,8 +2,8 @@ import "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js";
 import { Factory } from "./factory/factory.js";
 const factory = new Factory();
 
-//await factory.ordersList();
+await factory.ordersList();
 await factory.deliverymansList();
-//await factory.addingDeliveryMansOptions();
+await factory.addingDeliveryMansOptions();
 
 factory.init();
