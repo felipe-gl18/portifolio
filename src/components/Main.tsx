31c5520d5c3c83ae5b20e92eeef36b2e8a7bce73
Felipe Lino <felipegadelha2004@gmail.com>
2025-12-06 22:28:31 -0300

    feat: add Main component
    Present myself, ocupation, description

---
+++ src/components/Main.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const StyledMain = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  background-color: #f5f5f5;
+  padding: 64px;
+
+  h1 {
+    color: #0d3b66;
+    font-size: 56px;
+    font-weight: 700;
+  }
+
+  span {
+    color: #555555;
+    font-size: 28px;
+    font-weight: 500;
+  }
+
+  p {
+    text-align: start;
+    color: #777777;
+    font-size: 18px;
+    font-weight: 500;
+    max-width: 740px;
+  }
+`;
+
+export default function Main() {
+  return (
+    <StyledMain>
+      <h1>Felipe Lino</h1>
+      <span>Software Engineer</span>
+      <p>
+        I’m a full-stack developer focused on building modern, scalable, and
+        maintainable web applications — especially in the SaaS and real-time
+        communication space. I’m passionate about solving real problems through
+        clean architecture, thoughtful design, and reliable engineering.
+      </p>
+    </StyledMain>
+  );
+}
